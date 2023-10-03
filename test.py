b5315da7ddf6e8b4882d5848986e00df72033941
Targary1 <luc.f.oswald@icloud.com>
2023-10-03 18:07:45 +0200

    new modules

---
+++ test.py
@@ -0,0 +1,8 @@
+
+prepaid = 50 + 100 + 500 + 200 + 200 + 200
+holiday = 364 + 75 + 390 + 172
+credit = 441
+paypal_parties = 29 + 11 + 21 + 21
+
+print(prepaid + holiday + credit +paypal_parties)
+print(holiday)
